9528edc2fa14700b5a95f4c393dbdaa36b033a08
Fathima Hida <fathimahidaktpm@gmail.com>
2026-09-16 10:18:37 +0530

    Final password checker - strong/weak working

--- password_checker.py
+++ password_checker.py
@@ -19,3 +19,17 @@ if has_capital == True:
     print('has capital')
 else:
     print('no capital')
+
+has_character = False
+for letter in password:
+    if letter in '!@#$%^&*':
+        has_character = True
+if has_character == True:
+    print('has character')
+else:
+    print('no character')
+
+if len(password) >= 8 and has_number and has_capital and has_character:
+    print('Strong password')
+else:
+    print('weak password')
